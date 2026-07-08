2474bbb7a1e6134fb61dffd05266e0973e9a999b
Andy He <andy-he@hotmail.com>
2026-07-08 09:03:03 -0300

    Task 3: waveFoam complete, ran to t=12s, U_current=0.2m/s, H=0.05m

--- task1_regularFlume/make_gif_task1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dirfold="New Folder"
-fprefix="task1_animation"
-figs="${dirfold}/${fprefix}*.png"
-moviefile="task1_birdseye_animation.gif"
-convert -delay 10 -loop 0 ${figs} ${moviefile}
